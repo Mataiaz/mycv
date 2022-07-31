dbc09981d0d2f2d033eb77c46363077b19633fec
Mataiaz <mathias344@live.no>
2022-07-31 03:34:07 +0200

    added new ref pics

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import CardItem from './CardItem';
 import img from '../assets/images/img1.jpg'
 import img1 from '../assets/images/homepage.jpg'
 import img2 from '../assets/images/screenshot4.png'
-import img3 from '../assets/images/picwebsite.png'
+import img3 from '../assets/images/snip3.png'
 
 
 function Cards() {
