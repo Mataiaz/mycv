ee7feee67624188a48cd4e7e880dd74bfd7a2ba5
Mataiaz <mathias344@live.no>
2022-08-04 23:19:43 +0200

    fixed logic error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,33 @@ import CommentForm from '../components/CommentForm';
 import { useCommentsContext } from '../hooks/useCommentsContext'
 
 function Home() {
+  const {comments, dispatch} = useCommentsContext()
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const response = await fetch('/api/comments')
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({ type: 'SET_COMMENTS', payload: json })
+      }
+    }
+    fetchComments()
+  }, [dispatch])
+
   return (
     <>
       <HeroSection />
       <Cards />
+      <div className='home'>
+        <div className='comments'>
+          {comments && comments.map(comment => (
+            <CommentDetails key={comment._id} comment={comment} />
+          ))}
+        </div>
+        <CommentForm />
+      </div>
+
       <Footer />
     </>
   );
