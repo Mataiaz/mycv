d8f6cd7150cab406f1b6b6bbd2450cac401c2c96
Mataiaz <mathias344@live.no>
2022-08-07 16:11:35 +0200

    made the route names for login and signup

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('exoress')
+
+const router = express.Router()
+
+//login route
+router.post('/login', () => {})
+
+//signup route
+router.post('/signup', () => {})
+
+
+module.exports = router
