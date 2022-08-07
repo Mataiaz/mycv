ba9f203962c9e0bdbc9e7eae5505cc08a9480e66
Mataiaz <mathias344@live.no>
2022-08-07 17:39:23 +0200

    added auth context provider

--- src/hooks/useCommentsContext copy.js
+++ src/hooks/useCommentsContext copy.js
@@ -5,7 +5,7 @@ export const useCommentsContext = () => {
     const context = useContext(CommentsContext)
 
     if(!context) {
-        throw Error('use context inside context provider')
+        throw Error('use context comment inside comment context provider')
     }
 
     return context
